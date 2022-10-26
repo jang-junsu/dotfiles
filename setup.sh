90124de9041ed724ad3d9a5994df799fb8487efa
jang-junsu <ko94deveric@gmail.com>
2022-10-27 01:33:34 +0900

    Fix: update setup script comments

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ GITCONFIG_FILE=~/.gitconfig
 ZSHRC=~/.zshrc
 
 
+# Clear exist rc and gitconfig file
 if [ -e  $VIMRC_FILE ]; then
   echo "${ORANGE}Remove exist $VIMRC_FILE"
   rm $VIMRC_FILE
@@ -22,21 +23,21 @@ fi
 
 # Setup rcs
 
-# Setup Env varivable for rc file path
+# Setup env varivable for rc file path
 echo "${ORANGE}Set up env variable for rc file path"
 echo "export RCPATH=~/dotfiles" >> $ZSHRC
 source $ZSHRC
 
+# Set up vim
 echo "${ORANGE}Setup Vimrc"
 ln -s $RCPATH/vimrc $VIMRC_FILE
 
 # Setup gitconfig
 echo "${ORANGE}Setup git config"
 ln -s $RCPATH/gitconfig $GITCONFIG_FILE 
-
 echo "${GREEN}Please open .gitconfig and fill user attributes"
 
-# setup aliases
+# Setup aliases
 echo "${ORANGE}Setup Aliases"
 echo "source $RCPATH/aliasrc" >> $ZSHRC 
 source $ZSHRC 
